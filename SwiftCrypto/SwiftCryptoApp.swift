190f2302c78a2cc0489114333ddc947e835bca63
harsha960 <90449551+harsha960@users.noreply.github.com>
2023-08-03 02:59:14 +0530

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Harsha Vardhan Ravipati on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
